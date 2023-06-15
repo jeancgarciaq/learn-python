a482afca9909166a6fed3727619fd80d97a65103
jeancgarciaq <jcvictory@hotmail.com>
2023-06-15 13:46:54 -0400

    Create introduction_function.py
    Ejemplo de una función sin argumentos

---
+++ introduction_function.py
@@ -0,0 +1,13 @@
+def rocket_parts():
+    print("payload, propellant, structure")
+
+rocket_parts()
+
+output = rocket_parts()
+output
+
+def rocket_parts():
+    return "payload, propellant, structure"
+
+output = rocket_parts()
+output
