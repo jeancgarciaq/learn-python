3b7ca44a47a743fb70ea4968d59f0d411e37e4c4
jeancgarciaq <jcvictory@hotmail.com>
2023-06-15 16:37:27 -0400

    Create red_square.py
    Cálculo del área de un cuadrado a partir de un arco

---
+++ red_square.py
@@ -0,0 +1,14 @@
+# ****************
+# EL CUADRADO ROJO
+# ****************
+
+
+def run(arc_A: float) -> float:
+    # TU C�DIGO AQU�
+    area = (arc_A**2)/2
+
+    return area
+
+
+if __name__ == '__main__':
+    run(1)
