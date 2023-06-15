c6d89e7b02b01a64634f059b226fba56b7be6ad2
jeancgarciaq <jcvictory@hotmail.com>
2023-06-15 13:49:13 -0400

    Create move_twice.py
    Ejemplo lanzamiento de un dado por 2, determinar el movimiento de una pieza de un juego dada su posición en un tablero lineal

---
+++ move_twice.py
@@ -0,0 +1,15 @@
+# *******************
+# TIRO PORQUE ME TOCA
+# *******************
+
+
+def run(current_pos: int, dice: int) -> int:
+    # TU C�DIGO AQU�
+    
+    final_pos = current_pos + (dice * 2)
+
+    return final_pos
+
+
+if __name__ == '__main__':
+    run(3, 6)
