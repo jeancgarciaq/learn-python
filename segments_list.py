8ed25834b249f0f61535f3f650fbd629804fcd07
jeancgarciaq <jcvictory@hotmail.com>
2023-06-15 16:32:37 -0400

    Create segments_list.py
    Ejemplo de listas en python

---
+++ segments_list.py
@@ -0,0 +1,11 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+
+# Output: ['Mercury', 'Venus']
+
+planets_after_earth = planets[3:8]
+print(planets_after_earth) 
+
+# Output
+# ['Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
