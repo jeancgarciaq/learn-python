a75a9f02ab7b2273cae14d34876af31f10f51dea
jeancgarciaq <jcvictory@hotmail.com>
2023-06-18 23:03:51 -0400

    Ejercicio convertir lista a lista caracteres

---
+++ chars_list.py
@@ -0,0 +1,15 @@
+# **********************
+# DESPLEGANDO CARACTERES
+# **********************
+
+
+def run(texts: list) -> list:
+    # TU C�DIGO AQU�
+    cadenas = "".join(texts)
+    chars = list(cadenas)
+
+    return chars
+
+
+if __name__ == '__main__':
+    run(['uno', 'dos', 'tres'])
